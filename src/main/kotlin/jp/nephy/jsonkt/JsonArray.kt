20601531fb74809af3ec8f29a15471259cdddb89
Nep <spica@starry.blue>
2018-02-21 19:01:24 +0900

    Add: JsonObject, JsonArrayを編集するメソッドを追加

--- src/main/kotlin/jp/nephy/jsonkt/JsonArray.kt
+++ src/main/kotlin/jp/nephy/jsonkt/JsonArray.kt
@@ -1,6 +1,7 @@
 package jp.nephy.jsonkt
 
 import com.google.gson.JsonArray
+import com.google.gson.JsonElement
 
 fun jsonArray(vararg elements: Any?): JsonArray {
     val json = JsonArray()
@@ -12,3 +13,17 @@ fun jsonArray(vararg elements: Any?): JsonArray {
 }
 
 fun JsonArray.contains(value: Any) = contains(value.toJsonElement())
+
+fun JsonArray.add(value: Any?) = add(value.toJsonElement())
+
+fun JsonArray.addAll(vararg values: Any?) = values.forEach { add(it) }
+fun JsonArray.addAll(values: Iterable<Any?>) = values.forEach { add(it) }
+fun JsonArray.addAll(values: Sequence<Any?>) = values.forEach { add(it) }
+
+fun JsonArray.removeAll(vararg values: JsonElement) = values.forEach { remove(it) }
+fun JsonArray.removeAll(values: Iterable<JsonElement>) = values.forEach { remove(it) }
+fun JsonArray.removeAll(values: Sequence<JsonElement>) = values.forEach { remove(it) }
+
+fun JsonArray.removeAllIndexes(vararg indexes: Int) = indexes.forEach { remove(it) }
+fun JsonArray.removeAllIndexes(indexes: Iterable<Int>) = indexes.forEach { remove(it) }
+fun JsonArray.removeAllIndexes(indexes: Sequence<Int>) = indexes.forEach { remove(it) }
