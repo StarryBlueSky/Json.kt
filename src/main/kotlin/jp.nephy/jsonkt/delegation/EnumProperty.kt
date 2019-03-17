02ba2c9b9a5a106b9e24765f4591740dad0ebc0c
Nep <spica@starry.blue>
2019-03-17 22:20:34 +0900

    Add copyrights

--- src/main/kotlin/jp.nephy/jsonkt/delegation/EnumProperty.kt
+++ src/main/kotlin/jp.nephy/jsonkt/delegation/EnumProperty.kt
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 @file:Suppress("UNUSED", "DEPRECATION")
 
 package jp.nephy.jsonkt.delegation
@@ -21,7 +45,10 @@ interface DoubleJsonEnum: JsonEnum<Double>
 interface CharJsonEnum: JsonEnum<Char>
 interface StringJsonEnum: JsonEnum<String>
 
-class NoSuchEmumMemberException(val enumClass: KClass<*>, val value: Any): JsonKtException("Enum class ${enumClass.effectiveName} does not have member of value $value.")
+class NoSuchEmumMemberException(
+    @Suppress("UNUSED_PARAMETER") val enumClass: KClass<*>,
+    @Suppress("UNUSED_PARAMETER") val value: Any
+): JsonKtException("Enum class ${enumClass.effectiveName} does not have member of value $value.")
 
 /**
  * @throws NoSuchEmumMemberException
