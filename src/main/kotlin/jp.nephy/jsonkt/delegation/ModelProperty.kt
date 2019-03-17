eba531f317fe513a45cd44ba6e516d11898ae1ee
Slash Nephy <slash@nephy.jp>
2019-03-17 22:20:00 +0900

    Add modelOrDefault property

--- src/main/kotlin/jp.nephy/jsonkt/delegation/ModelProperty.kt
+++ src/main/kotlin/jp.nephy/jsonkt/delegation/ModelProperty.kt
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 @file:Suppress("UNUSED")
 
 package jp.nephy.jsonkt.delegation
@@ -9,15 +33,22 @@ interface JsonModel {
     val json: JsonObject
 }
 
-class InvalidJsonModelException(val modelClass: KClass<*>): JsonKtException("${modelClass.effectiveName} does not have valid constructor.")
+class InvalidJsonModelException(
+    @Suppress("UNUSED_PARAMETER") val modelClass: KClass<*>
+): JsonKtException("${modelClass.effectiveName} does not have valid constructor.")
 
 /**
  * @throws InvalidJsonModelException
  * @throws JsonCastException
  */
-inline fun <reified T: JsonModel> JsonObject?.jsonModelProperty(key: String? = null, vararg args: Any?) = jsonObjectProperty(key) { it.parse<T>(*args) }
+@PublishedApi
+internal inline fun <reified T: JsonModel> JsonObject?.jsonModelProperty(key: String? = null, vararg args: Any?) = jsonObjectProperty(key) { it.parse<T>(*args) }
 
-inline fun <reified T: JsonModel> JsonObject?.nullableJsonModelProperty(key: String? = null, vararg args: Any?) = jsonObjectProperty(key) { it.parseOrNull<T>(*args) }
+@PublishedApi
+internal inline fun <reified T: JsonModel> JsonObject?.jsonModelOrDefaultProperty(key: String? = null, vararg args: Any?) = jsonObjectProperty(key) { it.parseOrNull<T>(*args) ?: jsonObjectOf().parse(*args) }
+
+@PublishedApi
+internal inline fun <reified T: JsonModel> JsonObject?.jsonModelOrNullProperty(key: String? = null, vararg args: Any?) = jsonObjectProperty(key) { it.parseOrNull<T>(*args) }
 
 /*
     JsonModel
@@ -27,13 +58,17 @@ inline fun <reified T: JsonModel> JsonObject.byModel(key: String? = null, vararg
 
 inline fun <reified T: JsonModel> JsonModel.model(key: String? = null, vararg args: Any?) = json.jsonModelProperty<T>(key, *args)
 
+inline fun <reified T: JsonModel> JsonObject.byModelOrDefault(key: String? = null, vararg args: Any?) = jsonModelOrDefaultProperty<T>(key, *args)
+
+inline fun <reified T: JsonModel> JsonModel.modelOrDefault(key: String? = null, vararg args: Any?) = json.jsonModelOrDefaultProperty<T>(key, *args)
+
 /*
     JsonModel?
  */
 
-inline fun <reified T: JsonModel> JsonObject?.byNullableModel(key: String? = null, vararg args: Any?) = nullableJsonModelProperty<T>(key, *args)
+inline fun <reified T: JsonModel> JsonObject?.byNullableModel(key: String? = null, vararg args: Any?) = jsonModelOrNullProperty<T>(key, *args)
 
-inline fun <reified T: JsonModel> JsonModel.nullableModel(key: String? = null, vararg args: Any?) = json.nullableJsonModelProperty<T>(key, *args)
+inline fun <reified T: JsonModel> JsonModel.nullableModel(key: String? = null, vararg args: Any?) = json.jsonModelOrNullProperty<T>(key, *args)
 
 /**
  * @throws InvalidJsonModelException
@@ -41,7 +76,9 @@ inline fun <reified T: JsonModel> JsonModel.nullableModel(key: String? = null, v
  */
 inline fun <reified T: JsonModel> JsonObject?.jsonModelListProperty(key: String? = null, vararg args: Any?) = jsonArrayProperty(key) { it.parse<T>(*args) }
 
-inline fun <reified T: JsonModel> JsonObject?.nullableJsonModelListProperty(key: String? = null, vararg args: Any?) = jsonArrayProperty(key) { it.parseOrNull<T>(*args) }
+inline fun <reified T: JsonModel> JsonObject?.jsonModelListOrDefaultProperty(key: String? = null, vararg args: Any?) = jsonArrayProperty(key) { it.parseOrNull<T>(*args) ?: jsonObjectOf().parse(*args) }
+
+inline fun <reified T: JsonModel> JsonObject?.jsonModelListOrNullProperty(key: String? = null, vararg args: Any?) = jsonArrayProperty(key) { it.parseOrNull<T>(*args) }
 
 /*
     List<JsonModel>
@@ -51,10 +88,14 @@ inline fun <reified T: JsonModel> JsonObject.byModelList(key: String? = null, va
 
 inline fun <reified T: JsonModel> JsonModel.modelList(key: String? = null, vararg args: Any?) = json.jsonModelListProperty<T>(key, *args)
 
+inline fun <reified T: JsonModel> JsonObject.byModelListOrDefault(key: String? = null, vararg args: Any?) = jsonModelListOrDefaultProperty<T>(key, *args)
+
+inline fun <reified T: JsonModel> JsonModel.modelListOrDefault(key: String? = null, vararg args: Any?) = json.jsonModelListOrDefaultProperty<T>(key, *args)
+
 /*
     List<JsonModel?>
  */
 
-inline fun <reified T: JsonModel> JsonObject?.byNullableModelList(key: String? = null, vararg args: Any?) = nullableJsonModelListProperty<T>(key, *args)
+inline fun <reified T: JsonModel> JsonObject?.byNullableModelList(key: String? = null, vararg args: Any?) = jsonModelListOrNullProperty<T>(key, *args)
 
-inline fun <reified T: JsonModel> JsonModel.nullableModelList(key: String? = null, vararg args: Any?) = json.nullableJsonModelListProperty<T>(key, *args)
+inline fun <reified T: JsonModel> JsonModel.nullableModelList(key: String? = null, vararg args: Any?) = json.jsonModelListOrNullProperty<T>(key, *args)
