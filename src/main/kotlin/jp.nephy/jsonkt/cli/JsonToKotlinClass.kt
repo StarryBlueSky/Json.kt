ff784c34f464dc86fef2d9b3c1222e6d53289ad6
Slash Nephy <slash@nephy.jp>
2019-03-17 22:20:34 +0900

    Add copyrights

--- src/main/kotlin/jp.nephy/jsonkt/cli/JsonToKotlinClass.kt
+++ src/main/kotlin/jp.nephy/jsonkt/cli/JsonToKotlinClass.kt
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ *     Copyright (c) 2017-2019 Nephy Project Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 @file:Suppress("UNUSED")
 
 package jp.nephy.jsonkt.cli
@@ -46,6 +70,7 @@ fun String.toModelString(modelName: String? = null, printComments: Boolean? = nu
     return JsonToKotlinClass(toJsonObject()).convert(modelName, printComments)
 }
 
+@Suppress("LiftReturnOrAssignment")
 class JsonToKotlinClass internal constructor(private val json: JsonObject) {
     fun convert(targetModelName: String?, printComments: Boolean?): String {
         return buildString {
@@ -127,7 +152,7 @@ class JsonToKotlinClass internal constructor(private val json: JsonObject) {
                                             k to v.run {
                                                 first.first().apply {
                                                     if (second) {
-                                                        when (this) {
+                                                        @Suppress("LiftReturnOrAssignment") when (this) {
                                                             is JsonPrimitive -> isNullable = true
                                                             is JsonObject -> isNullable = true
                                                             is JsonArray -> isNullable = true
