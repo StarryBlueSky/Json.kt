ec44eae4d545e8e39e2cc2a33aef056704ae78e9
Nep <spica@starry.blue>
2019-04-11 15:52:35 +0900

    Remove unused deserializer

--- src/main/kotlin/jp.nephy/jsonkt/JsonKt.kt
+++ src/main/kotlin/jp.nephy/jsonkt/JsonKt.kt
@@ -30,8 +30,6 @@ import kotlin.reflect.KClass
 
 object JsonKt {
     internal val serializers = mutableMapOf<KClass<*>, Serializer<*>>()
-    // TODO
-    private val deserializers = mutableMapOf<KClass<*>, Deserializer<*>>()
 
     fun <T: Any> install(sourceClass: KClass<T>, serializer: Serializer<T>) {
         serializers[sourceClass] = serializer
@@ -40,18 +38,7 @@ object JsonKt {
         install(T::class, serializer)
     }
 
-    fun <T: Any> install(targetClass: KClass<T>, serializer: Deserializer<T>) {
-        deserializers[targetClass] = serializer
-    }
-    inline fun <reified T: Any> install(deserializer: Deserializer<T>) {
-        install(T::class, deserializer)
-    }
-
     interface Serializer<T: Any> {
         fun encode(data: T): String
-    }
-
-    interface Deserializer<T: Any> {
-        fun decode(data: String): T
     }
 }
