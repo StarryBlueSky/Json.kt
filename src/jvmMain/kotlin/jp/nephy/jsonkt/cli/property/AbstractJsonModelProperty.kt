011126bbac04c5939c04247564fda17053c8521f
Nep <spica@starry.blue>
2019-04-11 15:45:07 +0900

    Delete obsolete MPP

--- src/jvmMain/kotlin/jp/nephy/jsonkt/cli/property/AbstractJsonModelProperty.kt
+++
@@ -1,7 +0,0 @@
-package jp.nephy.jsonkt.cli.property
-
-import jp.nephy.jsonkt.JsonElement
-
-internal abstract class AbstractJsonModelProperty(pair: Map.Entry<String, JsonElement>, printComments: Boolean): AbstractJsonProperty(pair, printComments) {
-    val modelName = key.toSafeKotlinLiteral().toLowerCamelCase().capitalize()
-}
--- src/jvmMain/kotlin/jp/nephy/jsonkt/cli/property/JsonNullablePrimitiveProperty.kt
+++
@@ -1,7 +0,0 @@
-package jp.nephy.jsonkt.cli.property
-
-import jp.nephy.jsonkt.JsonElement
-
-internal class JsonNullablePrimitiveProperty(pair: Map.Entry<String, JsonElement>, printComments: Boolean): AbstractJsonProperty(pair, printComments) {
-    override val delegationName = toMethodName(jsonPrimitive = element.primitive, nullable = true)
-}
